c61e9def8d50ec0338d4caf7d44be1005af982f0
Ibroxim <ibrohimismoilov738@gmail.com>
2024-02-25 14:02:32 +0500

    changed text

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
           <div className="footer-lists-content">
             <ul>
               <li><Link><h1>about us</h1></Link></li>
-              <li>We are the online store with 10 years of experience.You can see more in about page</li>
+              <li>We are the online store with 10 years of experience.You can see more info in about page</li>
               <li>
                 <Link to={"tel:+998977041308"}>
                   <i className="bi bi-telephone-fill"></i>
