b24b3e733b99018adef0687cf0ce700bc4472b6e
Ibroxim <ibrohimismoilov738@gmail.com>
2024-02-25 12:13:21 +0500

    footer section have written

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,61 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className='footer-wrapper'>
+      <div className="footer-lists">
+        <div className="container">
+          <div className="footer-lists-content">
+            <ul>
+              <li><Link><h1>about us</h1></Link></li>
+              <li>We are the online store with 10 years of experience.You can see more in about page</li>
+              <li>
+                <Link to={"tel:+998977041308"}>
+                  <i className="bi bi-telephone-fill"></i>
+                  <span>+998977041308</span>
+                </Link>
+              </li>
+              <li>
+                <Link to={"mailto:ibrohimismoilov738@gmail.com"}>
+                  <i className="bi bi-envelope"></i>
+                  <span>ibrohimismoilov738@gmail.com</span>
+                </Link>
+              </li>
+              <li>
+                <Link to={"https://maps.app.goo.gl/hYbxDNCSvPmqhMMm7"}>
+                  <i className="bi bi-geo-alt-fill"></i>
+                  <span>Tashkent, Dormon street 35</span>
+                </Link>
+              </li>
+            </ul>
+            <ul>
+              <li><h1>categories</h1></li>
+              <li><Link>All Products</Link></li>
+              <li><Link>Laptops</Link></li>
+              <li><Link>Furniture</Link></li>
+              <li><Link>Motorcycle</Link></li>
+            </ul>
+            <ul>
+              <li><h1>information</h1></li>
+              <li><Link>About Us</Link></li>
+              <li><Link>Contact Us</Link></li>
+              <li><Link>Privacy Policy</Link></li>
+              <li><Link>Orders and Returns</Link></li>
+            </ul>
+            <ul>
+              <li><h1>service</h1></li>
+              <li><Link>My Account</Link></li>
+              <li><Link>View Cart</Link></li>
+              <li><Link>Wishlist</Link></li>
+              <li><Link>Help</Link></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <section className='footer-end'>
+        <span>Copyright &copy; 2024 All rights reserved | This is made by <b>Ibroxim</b></span>
+      </section>
+    </footer>
   )
 }
