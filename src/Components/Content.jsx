0e24d7017cf8cfe600e64aa800e5141c958df027
Ibroxim <ibrohimismoilov738@gmail.com>
2024-02-25 10:54:31 +0500

    created one more route page which is default route page

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -5,6 +5,7 @@ import Home from '../Pages/Home'
 export default function Content() {
   return (
     <Routes>
+      <Route path='/' element={<Home />} />
       <Route path='/home' element={<Home />} />
     </Routes>
   )
