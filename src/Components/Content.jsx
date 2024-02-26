95755c8c56ed4ee41d2060d9cc330e71a0548c07
Ibroxim <ibrohimismoilov738@gmail.com>
2024-02-26 09:01:59 +0500

    added catefory products page

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../Pages/Home'
 import SingleProduct from '../Pages/SingleProduct'
+import CategoryProducts from '../Pages/CategoryProducts'
 
 export default function Content() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/home' element={<Home />} />
+      <Route path='/categories/:category' element={<CategoryProducts />} />
       <Route path='/product/:id' element={<SingleProduct />} />
     </Routes>
   )
