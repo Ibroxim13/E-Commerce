45866ff8a3650a4b9c269745c6fd49656d64f790
Ibroxim <ibrohimismoilov738@gmail.com>
2024-02-26 15:18:45 +0500

    imported ContextProvider,ToastContainer and css of toastify

--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './Components/App';
 import "./Style/style.css"
+import ContextProvider from './Context/MainContext';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <ContextProvider>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+      <App />
+    </ContextProvider>
   </BrowserRouter>
 )
 
