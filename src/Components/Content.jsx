5faf28b511fd9da56644f60a7ea1c5fb92096ebc
Ibroxim <ibrohimismoilov738@gmail.com>
2024-02-26 15:19:08 +0500

    added new wishlist route page

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom'
 import Home from '../Pages/Home'
 import SingleProduct from '../Pages/SingleProduct'
 import CategoryProducts from '../Pages/CategoryProducts'
+import WishList from '../Pages/WishList'
 
 export default function Content() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/home' element={<Home />} />
+      <Route path='/wishlist' element={<WishList />} />
       <Route path='/categories/:category' element={<CategoryProducts />} />
       <Route path='/product/:id' element={<SingleProduct />} />
     </Routes>
