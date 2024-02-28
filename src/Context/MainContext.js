77df8afc3e89e2a01ec8be9fde194ef572317fa8
Ibroxim <ibrohimismoilov738@gmail.com>
2024-02-28 09:05:26 +0500

    created new states and exported

--- src/Context/MainContext.js
+++ src/Context/MainContext.js
@@ -1,4 +1,4 @@
-import { useContext, createContext, useState, useEffect } from "react";
+import { useContext, createContext, useState } from "react";
 
 const Context = createContext()
 export const useContextProvider = () => useContext(Context)
@@ -6,9 +6,17 @@ export const useContextProvider = () => useContext(Context)
 const ContextProvider = ({ children }) => {
     let [wishlist, setWishlist] = useState(JSON.parse(localStorage.getItem("wishlist")) ? JSON.parse(localStorage.getItem("wishlist")) : 0)
     let [wishlistProducts, setWishlistProducts] = useState(JSON.parse(localStorage.getItem("wishlist-products")) ? JSON.parse(localStorage.getItem("wishlist-products")) : [])
+    let [cartProducts, setCartProducts] = useState(JSON.parse(localStorage.getItem("cart-products")) ? JSON.parse(localStorage.getItem("cart-products")) : [])
+    let [cartProductsCount, setCartProductsCount] = useState(JSON.parse(localStorage.getItem("cart-products-count")) ? JSON.parse(localStorage.getItem("cart-products-count")) : 0)
 
     return (
-        <Context.Provider value={[wishlist, setWishlist, wishlistProducts, setWishlistProducts]}>
+        <Context.Provider value=
+            {[
+                wishlist, setWishlist,
+                wishlistProducts, setWishlistProducts,
+                cartProducts, setCartProducts,
+                cartProductsCount, setCartProductsCount
+            ]}>
             {children}
         </Context.Provider>
     )
