e9596beae921f1161d1bde8ac5e083d5243c40c0
Ibroxim <ibrohimismoilov738@gmail.com>
2024-02-29 08:11:32 +0500

    imported new route page

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -6,6 +6,7 @@ import CategoryProducts from '../Pages/CategoryProducts'
 import WishList from '../Pages/WishList'
 import MyAccount from '../Pages/MyAccount'
 import Signup from '../Pages/Signup'
+import Cart from '../Pages/Cart'
 
 export default function Content() {
   return (
@@ -13,6 +14,7 @@ export default function Content() {
       <Route path='/' element={<Home />} />
       <Route path='/home' element={<Home />} />
       <Route path='/wishlist' element={<WishList />} />
+      <Route path='/cart' element={<Cart />} />
       <Route path='/my-account' element={<MyAccount />} />
       <Route path='/sign-up' element={<Signup />} />
       <Route path='/categories/:category' element={<CategoryProducts />} />
